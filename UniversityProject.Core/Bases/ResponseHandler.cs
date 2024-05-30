18f8f9e9f5320383cb6136d70935445a11ea98ff
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-30 13:12:21 +0300

    Handle Responses

--- UniversityProject.Core/Bases/ResponseHandler.cs
+++ UniversityProject.Core/Bases/ResponseHandler.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniversityProject.Core.Bases
+﻿namespace UniversityProject.Core.Bases;
+public class ResponseHandler
 {
-    internal class ResponseHandler
+    public ResponseHandler()
     {
+
+    }
+    public Response<T> Deleted<T>()
+    {
+        return new Response<T>()
+        {
+            StatusCode = System.Net.HttpStatusCode.OK,
+            Succeeded = true,
+            Message = "Deleted Successfully"
+        };
+    }
+    public Response<T> Success<T>(T entity, object Meta = null)
+    {
+        return new Response<T>()
+        {
+            Data = entity,
+            StatusCode = System.Net.HttpStatusCode.OK,
+            Succeeded = true,
+            Message = "Added Successfully",
+            Meta = Meta
+        };
+    }
+    public Response<T> Unauthorized<T>()
+    {
+        return new Response<T>()
+        {
+            StatusCode = System.Net.HttpStatusCode.Unauthorized,
+            Succeeded = true,
+            Message = "UnAuthorized"
+        };
+    }
+    public Response<T> BadRequest<T>(string Message = null)
+    {
+        return new Response<T>()
+        {
+            StatusCode = System.Net.HttpStatusCode.BadRequest,
+            Succeeded = false,
+            Message = Message == null ? "Bad Request" : Message
+        };
+    }
+
+    public Response<T> NotFound<T>(string message = null)
+    {
+        return new Response<T>()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound,
+            Succeeded = false,
+            Message = message == null ? "Not Found" : message
+        };
+    }
+
+    public Response<T> Created<T>(T entity, object Meta = null)
+    {
+        return new Response<T>()
+        {
+            Data = entity,
+            StatusCode = System.Net.HttpStatusCode.Created,
+            Succeeded = true,
+            Message = "Done Created",
+            Meta = Meta
+        };
     }
 }
