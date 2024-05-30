b79ea359fa251ab8c8b74e5fd4fd416e1455cb3f
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-05-30 13:42:40 +0300

    add genericRepo & Abstracts Methods

---
+++ UniversityProject.Infrustructure/GenericRepository/GenericRepositoryAsync.cs
@@ -0,0 +1,72 @@
+﻿
+using Microsoft.EntityFrameworkCore.Storage;
+using UniversityProject.Infrustructure.IGenericRepository;
+
+namespace UniversityProject.Infrustructure.GenericRepository;
+public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+{
+    public Task<T> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddRangeAsync(ICollection<T> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDbContextTransaction BeginTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Commit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteRangeAsync(ICollection<T> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> GetTableAsTracking()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<T> GetTableNoTracking()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RollBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateRangeAsync(ICollection<T> entities)
+    {
+        throw new NotImplementedException();
+    }
+}
