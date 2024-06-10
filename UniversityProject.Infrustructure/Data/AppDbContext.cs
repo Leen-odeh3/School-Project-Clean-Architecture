ae2ae6ff3af8ac1f5156f0ac56855ae9e480db8a
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-10 17:54:01 +0300

    Refactor to use fluent api

--- UniversityProject.Infrustructure/Data/AppDbContext.cs
+++ UniversityProject.Infrustructure/Data/AppDbContext.cs
@@ -11,22 +11,6 @@ public class AppDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<DepartmentCourse>().HasKey(dc => new { dc.DepartmentID, dc.CourseID });
-        modelBuilder.Entity<InstructorCourse>().HasKey(ic => new { ic.InstructorID, ic.CourseID });
-        modelBuilder.Entity<StudentCourse>().HasKey(sc => new { sc.StudentID, sc.CourseID });
-
-        modelBuilder.Entity<Department>()
-                    .HasMany(d => d.Instructors)
-                    .WithOne(i => i.department)
-                    .HasForeignKey(i => i.DepartmentID)
-                    .IsRequired(false);
-
-        modelBuilder.Entity<Department>()
-                    .HasOne(d => d.Instructor)
-                    .WithOne()
-                    .HasForeignKey<Department>(d => d.InsManager)
-                    .IsRequired(false);
-
         base.OnModelCreating(modelBuilder);
     }
 
